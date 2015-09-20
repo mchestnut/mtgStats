db5df665a8b3654155ca3b76c2e35b98ae57f4fe
Matt Chestnut <mchestnut85@gmail.com>
2015-09-19 17:21:57 -0700

    Fixed Grunt process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,21 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
  
         clean: ["dist", '.tmp'],
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'app/_sass/bootstrap',
+                    cssDir: 'app/css',
+                    imagesDir: 'app/img',
+                    generatedImagesDir: 'app/img',
+                    environment: 'production',
+                    outputStyle: 'compressed',
+                    require: '',
+                    relativeAssets: true
+                }
+            }
+        },
  
         copy: {
             main: {
@@ -39,14 +54,23 @@ module.exports = function (grunt) {
                 report: 'min',
                 mangle: false
             }
+        },
+
+        watch: {
+            css: {
+                files: 'app/**/*.scss',
+                tasks: ['compass']
+            }
         }
     });
  
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-rev');
     grunt.loadNpmTasks('grunt-usemin');
  
